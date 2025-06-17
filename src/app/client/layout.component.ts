124eba084e59d4b9481a07ba89592edb2bd554d8
khaledmo77 <khaledmo77@outlook.com>
2025-06-17 20:58:20 +0300

    enhanced footer and header

--- src/app/client/layout.component.ts
+++ src/app/client/layout.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet,RouterLink, RouterLinkActive } from '@angular/router';
 import { NavbarComponent } from '../shared/navbar/navbar.component'; 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { NgIf } from '@angular/common';
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet, MatToolbarModule, MatButtonModule],
+  imports: [  RouterOutlet,
+    RouterLinkActive,
+    MatToolbarModule,
+    RouterLink,
+    MatGridListModule,
+    MatIconModule,
+    MatListModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
