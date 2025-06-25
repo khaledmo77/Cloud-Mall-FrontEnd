4b3217e0d4d50a0e501ff5009c20f64150dc4aa6
sherineAhmedd <shereenahmed497@gmail.com>
2025-06-25 13:17:27 +0300

    mode in navbar

--- src/app/client/store-list/store-list.component.ts
+++ src/app/client/store-list/store-list.component.ts
@@ -13,59 +13,88 @@ export class StoreListComponent {
     id: 1,
     name: 'store 1',
     image: 'assets/img/gallery/11.jpg',
-    description: 'This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.'
+     category: 'fashion',
+    description: 'This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
+   
   },
   {
     id: 2,
     name: 'store 2',
     image: 'assets/img/gallery/11.jpg',
-    description: 'This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.'
+    category: 'fashion',
+    description: 'This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
+     
   },
   {
     id: 3,
     name: 'store 3',
     image: 'assets/img/gallery/11.jpg',
+     category: 'fashion',
     description: 'This is a longer card with supporting text below as a natural lead-in to additional content.'
   },
   {
     id: 4,
     name: 'store 4',
     image: 'assets/img/gallery/11.jpg',
+     category: 'fashion',
     description: 'This is a longer card with supporting text below as a natural lead-in to additional content.'
   },
   {
     id: 5,
     name: 'store 5',
     image: 'assets/img/gallery/11.jpg',
+     category: 'fashion',
     description: 'This is a longer card with supporting text below as a natural lead-in to additional content.'
   },
   {
     id: 6,
     name: 'store 6',
     image: 'assets/img/gallery/11.jpg',
+     category: 'fashion',
     description: 'This is a longer card with supporting text below as a natural lead-in to additional content.'
   },
   {
     id: 7,
     name: 'store 7',
     image: 'assets/img/gallery/11.jpg',
+     category: 'fashion',
     description: 'This is a longer card with supporting text below as a natural lead-in to additional content.'
   },
     {
     id: 8,
     name: 'store 8',
     image: 'assets/img/gallery/11.jpg',
+     category: 'fashion',
     description: 'This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.'
   },
     {
     id: 9,
     name: 'store 9',
     image: 'assets/img/gallery/11.jpg',
+     category: 'fashion',
     description: 'This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.'
   },
 
 ];//create array of stores
-    
+    filteredStores :Store[]= [];
+
+     ngOnInit() {
+    this.filteredStores = this.stores; // default is all
+  }
+
+  
+  filterStores(category: string) {
+    if (category === 'all') {
+      this.filteredStores = this.stores;
+    } else {
+      this.filteredStores = this.stores.filter(store =>
+        store.category === category
+      );
+    }
+      // Scroll to the list section
+    document.getElementById('all-stores')?.scrollIntoView({ behavior: 'smooth' });
+  
+  }
   
 }
 interface Store{
@@ -73,4 +102,5 @@ interface Store{
     name: string;
     image: string;
     description: string;
+    category: string;
 }
