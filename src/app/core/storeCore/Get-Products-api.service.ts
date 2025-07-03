e2eef4dd0e8bf12eaf68f350dc8f0a6dc42acb9f
sherineAhmedd <shereenahmed497@gmail.com>
2025-07-03 20:17:08 +0300

    get Products

---
+++ src/app/core/storeCore/Get-Products-api.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class GetProductsApiService{
+
+    private baseUrl = '/api/product';
+  constructor(private http: HttpClient) {}
+
+//     getAllProducts(storeId: number): Observable<product[]> {
+//     return this.http.get<Product[]>(this.baseUrl);
+//   }
+
+}
--- src/app/shared/storePages/Storehome/Storehome.component.ts
+++ src/app/shared/storePages/Storehome/Storehome.component.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { HeroSlider } from '../../storeComponents/hero-slider/hero-slider';
+import { ProductApiService } from '../../../core/storeCore/product-api.service';
+import { CommonModule } from '@angular/common';
+import { GetProductsApiService } from '../../../core/storeCore/Get-Products-api.service';
+
 
 @Component({
   selector: 'app-Storehome',
   standalone: true,
-  imports: [RouterLink, HeroSlider],
+  imports: [RouterLink, HeroSlider, CommonModule],
   templateUrl: './Storehome.component.html',
   styleUrls: ['./Storehome.component.scss']
 })
-export class Storehome {}
+export class Storehome {
+    storeId: number = 0;
+    products: any[] = [];
+
+     constructor( private route: ActivatedRoute, 
+     private productService: ProductApiService
+  ) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      const id = params.get('storeId');
+      if (id) {
+        this.storeId = +id;
+        this.loadProducts();
+      }
+    });
+  }
+   loadProducts(): void {
+    this.productService.getProductsByStore(this.storeId).subscribe({
+      next: (data) => {
+        this.products = data;
+      },
+      error: (err) => {
+        console.error('Error loading store products:', err);
+      }
+    });
+  }
+}
