fb32094cebbf2bca5a06427524ceae302b9b0fb3
sherineAhmedd <shereenahmed497@gmail.com>
2025-07-05 18:43:17 +0300

    add cart

--- src/app/core/ClientCore/client-cart-api.service.ts
+++ src/app/core/ClientCore/client-cart-api.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
-interface CartItem {
+export interface CartItem {
   productId: number;
   productName: string;
   price: number;
@@ -19,7 +19,19 @@ export class CartService{
      private cartSubject = new BehaviorSubject<CartItem[]>([]);//holds the cart items as first empty cart.
 
      cart$ = this.cartSubject.asObservable(); //"observable"to protect the cart state from being changed
-
+     
+      constructor() {
+    // Load cart from localStorage if it exists
+    const savedCart = localStorage.getItem('cart');
+    if (savedCart) {
+      this.cart = JSON.parse(savedCart);
+      this.cartSubject.next(this.cart);
+    }
+  }
+   private updateCartState() {
+    this.cartSubject.next(this.cart); // update observable
+    localStorage.setItem('cart', JSON.stringify(this.cart)); // save in storage
+  }
      addToCart(product: CartItem){   //to add products to cart
           const existing = this.cart.find(p => p.productId === product.productId);//to get product with same id i found so will be saved in existing variable 
            if (existing) {
@@ -27,7 +39,7 @@ export class CartService{
            } else {
            this.cart.push(product);//not found so add it for first time
            }
-          this.cartSubject.next(this.cart);//your cart page count stays in sync ,subscriber that the cart was updated.
+           this.updateCartState();
      }
 
      removeFromCart(productId: number){
--- src/app/shared/storeComponents/header/StoreHeader.component.ts
+++ src/app/shared/storeComponents/header/StoreHeader.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CartService } from '../../../core/ClientCore/client-cart-api.service';
+import { CartItem } from '../../../core/ClientCore/client-cart-api.service'; // update the path as needed
 
 @Component({
   selector: 'app-StoreHeader',
@@ -8,6 +10,27 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './StoreHeader.component.html',
   styleUrl: './StoreHeader.component.scss'
 })
-export class StoreHeader {
+export class StoreHeader implements OnInit {
+  cartItems: CartItem[] = [];
+  totalCount = 0;
+  totalAmount =0;
+
+   constructor(private cartService: CartService,) {}
+
+   ngOnInit(): void {
+       this.cartService.cart$.subscribe(items => {
+      this.cartItems = items;
+      this.totalCount = items.reduce((sum, item) => sum + item.quantity, 0);
+      this.totalAmount = items.reduce((sum, item) => sum + item.price * item.quantity, 0);
+    });
+   }
+
+   
+  removeItem(productId: number) {
+    this.cartService.removeFromCart(productId);
+  }
+   clearCart() {
+    this.cartService.cleanCart();
+  }
 
 }
